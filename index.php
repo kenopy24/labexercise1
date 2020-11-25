4ca9e7af9ffe66a152da722534dda571d887a6e3
kenopy24 <60841467+kenopy24@users.noreply.github.com>
2020-11-25 21:19:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <html>
-  <head>
-    <title>Display Hello World</title>
-  </head>
-  
   <body>
     <h1>Hello World</h1>
+    <p>Welcome to my world!</p>
+    <p>Welcome to my world!</p>
     <p>Welcome to my world!</p>
   </body>
 </html>
